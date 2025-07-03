3bfad0f3fc31ba8bf91e39b3b0232136a1977100
zhangge <zhangge@syriusrobotics.com>
2025-07-03 20:53:37 +0800

    增加AS5047笔记

--- 常用软件/format.c
+++ 常用软件/format.c
@@ -1,19 +1,28 @@
 void test_fun(void)
 {
-  int a = 0;
-  if(a) {}
+	int a = 0;
+	if (a) {
+	}
 }
+struct foc_data {
+	uint8_t a,
+};
+
+float test_arry[6] = {
+	1, 2, 3, 4, 5, 6,
+};
+float test_arry1[][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
 int main(void)
 {
-  void *p = NULL;
-  int a = 0;
-  test_fun();
-  if(a != 1) {
-    p = (void *)&a;
-  } else if(a == 1) {
-    p = (void *)&a;
-  } else {
-    a = 2;
-  }
-  return 1;
+	void *p = NULL;
+	int a = 0;
+	test_fun();
+	if (a != 1) {
+		p = (void *)&a;
+	} else if (a == 1) {
+		p = (void *)&a;
+	} else {
+		a = 2;
+	}
+	return 1;
 }
